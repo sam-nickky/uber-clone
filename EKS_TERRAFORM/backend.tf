fc2d5fca93a6d61fdf127aee5ef8bdb23c1e27d0
sam-nickky <103401075+sam-nickky@users.noreply.github.com>
2024-12-31 21:37:07 +0530

    Update backend.tf
    c

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,7 +1,13 @@
+
 terraform {
   backend "s3" {
     bucket = "ajay-mrcloudbook7771" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
+    shared_credentials_file = "~/.aws/credentials"
   }
+}
+provider "aws" {
+  region                  = "us-east-1"
+  shared_credentials_file = "~/.aws/credentials"
 }
